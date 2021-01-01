e65ca616a0d07fb846da6b6419162629e0ecb46d
kylehovey <khoveyc@gmail.com>
2020-12-31 18:17:44 -0800

    Simplified serial port mock

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-class MockSerial {
+const port = {
   on(_, fn) {
     setInterval(() => fn(Math.random()), 1000);
   }
-}
-
-const port = new MockSerial();
+};
 
 io.on('connection', (socket) => {
   port.on("data", (data) => socket.emit("data", data));
