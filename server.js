67f4d7369b1eae6c7a0e3148602fff4e50a9fd94
kylehovey <khoveyc@gmail.com>
2021-01-02 10:57:41 -0800

    Serving static build assets from Express

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-const app = require('express')();
-const server = require('http').createServer(app);
+const path = require('path');
+const express = require('express');
+const http = require('http');
 const cors = require('cors');
 const morgan = require('morgan');
 
 const apollo = require('./app/graphql');
 
+const app = express();
+const server = http.createServer(app);
+
 app.use('*', cors({ origin: 'http://localhost:3000' }));
 app.use(morgan('combined'));
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 apollo.applyMiddleware({ app });
 apollo.installSubscriptionHandlers(server);
