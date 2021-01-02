cc9d4ce3671a1a807c112714264761df29f6eda4
kylehovey <khoveyc@gmail.com>
2021-01-02 11:15:54 -0800

    No longer using CORS (Apollo Server) handles that

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const http = require('http');
-const cors = require('cors');
 const morgan = require('morgan');
 
 const apollo = require('./app/graphql');
@@ -12,12 +11,6 @@ const app = express();
 const server = http.createServer(app);
 
 app.use(morgan('combined'));
-
-app.use('*',
-  cors({
-    origin: `http://localhost:3000`,
-  }),
-);
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
